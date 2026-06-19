dc69691c7f065ff2634689e00aa0691c5f0436b3
Pedro Marchetti Casteli <pedrocasteli68@gmail.com>
2026-06-19 14:05:20 -0300

    Validate the destination and cabin class codes

--- faremenu.go
+++ faremenu.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	var origin, destination, cabinClass string
 
+	fmt.Println("*** Silver Karma Airfare Calculator")
+
 	validOriginEntered := false
 	var originCity City
 	var originErr error
-
-	fmt.Println("*** Silver Karma Airfare Calculator")
 
 	for !validOriginEntered {
 		fmt.Print("Enter origin code: ")
@@ -25,12 +25,41 @@ func main() {
 		}
 	}
 
-	fmt.Print("Enter destination code: ")
-	fmt.Scanln(&destination)
-	fmt.Println("You've entered " + destination)
+	validDestinationEntered := false
+	var destinationCity City
+	var destinationErr error
 
-	fmt.Print("Enter cabin class code: ")
-	fmt.Scanln(&cabinClass)
-	fmt.Println("You've entered " + cabinClass)
+	for !validDestinationEntered {
+		fmt.Print("Enter destination code: ")
+		fmt.Scanln(&destination)
+
+		destinationCity, destinationErr = getCityFromCode(destination)
+
+		if destinationErr == nil {
+			fmt.Println("You've entered " + destinationCity.cityName)
+			validDestinationEntered = true
+		} else {
+			fmt.Println(destinationErr)
+		}
+	}
+
+	validCabinClassEntered := false
+	var enteredCabinClass CabinClass
+	var enteredCabinClassErr error
+
+	for !validCabinClassEntered {
+
+		fmt.Print("Enter cabin class code: ")
+		fmt.Scanln(&cabinClass)
+
+		enteredCabinClass, enteredCabinClassErr = getCabinClassFromCode(cabinClass)
+
+		if enteredCabinClassErr == nil {
+			fmt.Println("You've entered " + enteredCabinClass.className + " class")
+			validCabinClassEntered = true
+		} else {
+			fmt.Println(enteredCabinClassErr)
+		}
+	}
 
 }
--- validate.go
+++ validate.go
@@ -16,3 +16,15 @@ func getCityFromCode(codeToLookup string) (City, error) {
 
 	return City{"", "", -999, -999}, errors.New(message)
 }
+
+func getCabinClassFromCode(codeToLookup string) (CabinClass, error) {
+	for _, item := range cabinClasses {
+		if item.code == codeToLookup {
+			return item, nil
+		}
+	}
+
+	message := fmt.Sprintf("%s is not a valid cabin class code", codeToLookup)
+
+	return CabinClass{"", "", -999}, errors.New(message)
+}
